71085f304b9c7f56e7335312fcff9b7dc54b4154
Srisharan03 <srisharan801@gmail.com>
2025-02-27 10:19:24 +0530

    Update clientCODE-IDS.c

--- clientCODE-IDS.c
+++ clientCODE-IDS.c
@@ -1,17 +1,44 @@
-import socket
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-HOST = '127.0.0.1'
-PORT = 65432
+#define PORT 3264
+#define SERVER_IP "127.0.0.1"
 
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((HOST, PORT))
+int main() {
+    int client_sock;
+    struct sockaddr_in server_addr;
+    char buffer[256];
 
-while True:
-    message = input("You: ")
-    client_socket.send(message.encode())
+    client_sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_sock == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
 
-    response = client_socket.recv(1024).decode()
-    print(f"Server: {response}")
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_IP);
 
-client_socket.close()
+    if (connect(client_sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
 
+    while (1) {
+        printf("Client: ");
+        fgets(buffer, sizeof(buffer), stdin);
+        write(client_sock, buffer, strlen(buffer));
+
+        if (strcmp(buffer, "exit\n") == 0) break;
+
+        memset(buffer, 0, sizeof(buffer));
+        read(client_sock, buffer, sizeof(buffer));
+        printf("Server: %s\n", buffer);
+    }
+
+    close(client_sock);
+    return 0;
+}
